84ea1c1565819e4d66968683b2b2630c85c1da28
s1n7ax <srineshnisala@gmail.com>
2020-06-05 14:21:20 +0530

    [UPD]

--- components/wifi.py
+++ components/wifi.py
@@ -3,6 +3,7 @@
 from dwm_status_events import trigger_change_event, on_signal
 from shell_exe import execute
 from threading import Thread
+from subprocess import CalledProcessError
 
 class Wifi:
     def __init__(self):
@@ -10,7 +11,10 @@ class Wifi:
         Thread(self.set_details()).start()
 
     def get_details(self):
-        ssid = execute([["iwgetid", "-r"]])
+        try:
+            ssid = execute([["iwgetid", "-r"]])
+        except CalledProcessError:
+            ssid = 'not connected'
         
         return "🖧  {}".format(ssid)
 
--- dwm_status_events.py
+++ dwm_status_events.py
@@ -5,12 +5,16 @@ import signal
 on_chage_callbacks = []
 on_signal_callbacks = set([])
 
+
 def add_changed_event_listener(callback):
     on_chage_callbacks.append(callback)
+
 
 '''
 runs on_chage_callbacks on event
 '''
+
+
 def trigger_change_event(org_func):
     def wrapper(self):
         org_func(self)
@@ -19,9 +23,12 @@ def trigger_change_event(org_func):
 
     return wrapper
 
+
 '''
 register status component functions to SIGUSR1 event
 '''
+
+
 def on_signal(callback):
     def wrapper(self):
         on_signal_callbacks.add(partial(callback, self))
