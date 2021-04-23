7e4111c027c7f9fdaa85ea7436aa438b7e3077fc
Luxraider <68786680+Luxraider@users.noreply.github.com>
2021-04-23 23:51:13 +0000

    Add files via upload

---
+++ components/battery.py
@@ -0,0 +1,24 @@
+#!/bin/python
+
+from subprocess import Popen,PIPE
+from dwm_status_events import trigger_change_event
+from shell_exe import execute
+from threading import Timer, Thread
+import subprocess
+
+class Battery:
+    def __init__(self):
+        self.details = ' 00.00%'
+        Thread(self.set_details()).start()
+
+    def get_details(self):
+        cpu = subprocess.check_output("cat /sys/class/power_supply/BAT0/capacity",shell=True)
+        return "BAT "+str(float(cpu))+"%"
+
+    @trigger_change_event
+    def set_details(self):
+        self.details = self.get_details()
+        Timer(2, self.set_details).start()
+
+    def __str__(self):
+        return self.details
